7999f8d0d065d4e72199189e8a6933d6cf96ec7c
Eddy <quenallataeddy@gmail.com>
2018-07-31 13:40:24 -0400

    motorizado pedir ubicacion

--- app/src/main/java/com/valecom/yingul/main/buy/BuyItemInfoReserveFragment.java
+++ app/src/main/java/com/valecom/yingul/main/buy/BuyItemInfoReserveFragment.java
@@ -2,6 +2,8 @@ package com.valecom.yingul.main.buy;
 
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -12,9 +14,15 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.google.gson.Gson;
 import com.valecom.yingul.R;
+import com.valecom.yingul.main.LoginActivity;
 import com.valecom.yingul.main.sell.SellActivity;
 import com.valecom.yingul.main.sell.SellItemAddUbicationFragment;
+import com.valecom.yingul.model.Yng_Ubication;
+import com.valecom.yingul.model.Yng_User;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +32,8 @@ public class BuyItemInfoReserveFragment extends Fragment {
     TextView txtShowInfo;
     private MaterialDialog setting_address_edit_dialog;
     private Button buttonSetInfo;
+    private Yng_User user;
+    private Yng_Ubication userUbication;
 
     public BuyItemInfoReserveFragment() {
         // Required empty public constructor
@@ -38,6 +48,30 @@ public class BuyItemInfoReserveFragment extends Fragment {
 
         txtShowInfo = (TextView) v.findViewById(R.id.txtShowInfo);
         buttonSetInfo = (Button) v.findViewById(R.id.buttonSetinfo);
+
+        user = new Yng_User();
+        SharedPreferences settings = getActivity().getSharedPreferences(LoginActivity.SESSION_USER, MODE_PRIVATE);
+        if (settings == null || settings.getInt("logged_in", 0) == 0 || settings.getString("api_key", "").equals(""))
+        {
+            Intent intent = new Intent(getContext(), LoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            getActivity().finish();
+        }else{
+            user.setUsername(settings.getString("username",""));
+            /*para obtener la ubicacion del usuario*/
+            if(settings.getString("yng_Ubication","").equals("null")){
+                userUbication=null;
+            }else{
+                Gson gson = new Gson();
+                userUbication = gson.fromJson(settings.getString("yng_Ubication","") , Yng_Ubication.class);
+            }
+            /*fin de la ubicacion del usuario*/
+            user.setPhone(settings.getString("phone",""));
+            user.setDocumentType(settings.getString("documentType",""));
+            user.setDocumentNumber(settings.getString("documentNumber",""));
+            user.setPassword(settings.getString("password",""));
+        }
 
         txtShowInfo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,12 +101,20 @@ public class BuyItemInfoReserveFragment extends Fragment {
         buttonSetInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((BuyActivity) getActivity()).shipping.setTypeShipping("home");
-                BuySetPaymentTypeFragment fragment = new BuySetPaymentTypeFragment();
-                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.content_frame, fragment);
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
+                if(userUbication==null||user.getPhone().equals("null")||user.getDocumentNumber().equals("null")||user.getDocumentType().equals("null")||user.getPhone().equals("")||user.getDocumentNumber().equals("")||user.getDocumentType().equals("")){
+                    Intent intent = new Intent(getContext(), BuyActivity.class);
+                    intent.putExtra("itemId",((BuyActivity)getActivity()).itemId);
+                    intent.putExtra("itemQuantity",((BuyActivity)getActivity()).quantity);
+                    startActivity(intent);
+                    getActivity().finish();
+                }else{
+                    ((BuyActivity) getActivity()).shipping.setTypeShipping("home");
+                    BuySetPaymentTypeFragment fragment = new BuySetPaymentTypeFragment();
+                    FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.content_frame, fragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                }
             }
         });
 
